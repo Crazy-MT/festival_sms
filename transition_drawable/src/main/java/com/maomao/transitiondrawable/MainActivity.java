201607bdaaa168e841548b9da49da7f16d395112
maotong <maotong@amberweather.com>
2016-01-23 23:45:26 +0800

    add transition drawable

--- transition_drawable/src/main/java/com/maomao/transitiondrawable/MainActivity.java
+++ transition_drawable/src/main/java/com/maomao/transitiondrawable/MainActivity.java
@@ -4,7 +4,9 @@ import android.content.res.Resources;
 import android.graphics.drawable.TransitionDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,5 +25,20 @@ public class MainActivity extends AppCompatActivity {
         img_transition = (ImageView) findViewById(R.id.img_transition);
         img_transition.setImageDrawable(transition);
         transition.startTransition(3000);
+
+        final ToggleButton toggleButton = (ToggleButton) findViewById(R.id.tb);
+        toggleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TransitionDrawable drawable = (TransitionDrawable) img_transition.getDrawable();
+                if (toggleButton.isChecked()) {
+                    drawable.startTransition(500);
+                } else {
+                    drawable.reverseTransition(500);
+                }
+            }
+        });
     }
+
+
 }
